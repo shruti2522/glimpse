b3bf3979be298386f1c9255d2ba7fe0db540f518
shruti2522 <shruti.apc01@gmail.com>
2023-10-25 12:00:00 +0530

    client deployment
    Signed-off-by: shruti2522 <shruti.apc01@gmail.com>

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -16,7 +16,7 @@ export const axiosConfig = () => {
  * EndPoints of the API used in the code
  */
 
-export const PROD_URL = `https://tourmaline-axolotl-1d1eae.netlify.app/` || `http://localhost:8000` ;
+export const PROD_URL = `https://glimpse-zy3s.onrender.com/` || `http://localhost:8000` ;
 // CreatePost Screen
 export const CREATE_POST_URL = `${PROD_URL}/createpost`;
 
